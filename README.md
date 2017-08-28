ee3eff9d2c0c04599fb3da46aad6751e9d6a9df9
Amritanshu Prasad <amri@imsc.res.in>
2017-08-28 13:34:12 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Assignment from 28th August 2017
+
+1. Add methods ``product`` to the class ComplexNumber
+provided in ``code.py``. Please make sure you have docstrings for all functions
+and methods
+
+2. Add an in-place method ``complex_conjugate``, which does not return anything,
+but replaces the instance by its complex conjugate.
+
+3. Add a method called ``polar_coordinates`` to ``NonZeroComplexNumber`` to return
+the polar coordinates. Use it to define a method called ``logarithm`` which gives 
+the principal branch of the log.
+
+**Do not use built-in methods of python for this. Docstrings are mandatory.**
---
+++ code.py
@@ -0,0 +1,52 @@
+from math import sqrt
+class ComplexNumber:
+    """
+    The class of complex numbers.
+    """
+    def __init__(self, real_part, imaginary_part):
+        """
+        Initialize ``self`` with real and imaginary part.
+        """
+        self.real = real_part
+        self.imaginary = imaginary_part
+    def __repr__(self):
+        """
+        Return the string representation of self.
+        """
+        return "%s + %s i"%(self.real, self.imaginary)
+    def __eq__(self, other):
+        """
+        Test if ``self`` equals ``other``.
+        
+        Two complex numbers are equal if their real parts are equal and
+        their imaginary parts are equal.
+        """
+        return self.real == other.real and self.imaginary == other.imaginary
+    def modulus(self):
+        """
+        Return the modulus of self.
+        
+        The modulus (or absolute value) of a complex number is the square
+        root of the sum of squares of its real and imaginary parts.
+        """
+        return sqrt(self.real**2 + self.imaginary**2)
+    def sum(self, other):
+        """
+        Return the sum of ``self`` and ``other``.
+        """
+        return ComplexNumber(self.real + other.real, self.imaginary + other.imaginary)
+
+class NonZeroComplexNumber(ComplexNumber):
+    def __init__(self, real_part, imaginary_part):
+        """
+        Initialize ``self`` with real and imaginary parts after checking validity.
+        """
+        if real_part == 0 and imaginary_part == 0:
+            raise ValueError("Real or imaginary part should be nonzero.")
+        return ComplexNumber.__init__(self, real_part, imaginary_part)
+    def inverse(self):
+        """
+        Return the multiplicative inverse of ``self``.
+        """
+        den = self.real**2 + self.imaginary**2
+        return NonZeroComplexNumber(self.real/den, -self.imaginary/den)
